b916b559a18e191e734940f3f8f3e10a510ba703
Ainul Hossain <99139055+Ainul-Hossain@users.noreply.github.com>
2023-06-22 21:19:49 +0600

    vercel api added

--- client/src/components/topbar/Topbar.js
+++ client/src/components/topbar/Topbar.js
@@ -8,7 +8,7 @@ import { Context } from "../../context/Context";
 
 const Topbar = () => {
   const {user, dispatch} = useContext(Context);
-  const PF = "http://localhost:5000/images/";
+  const PF = "https://mern-tutorials-blog-api.vercel.app/images/";
 
   const handleLogout = ()=>{
     dispatch({type: 'LOGOUT'})
