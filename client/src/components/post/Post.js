26a831445fba7a363d62ded59202c59a0f3e11cd
Ainul Hossain <99139055+Ainul-Hossain@users.noreply.github.com>
2023-06-22 21:14:59 +0600

    vercel api added

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -5,7 +5,7 @@ import {Link} from 'react-router-dom';
 const Post = ({post}) => {
     // console.log(post);
 
-    const PF = "http://localhost:5000/images/";
+    const PF = "https://mern-tutorials-blog-api.vercel.app/images/";
 
     return (
         <div className='post'>
